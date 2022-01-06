9cc89a601fea6d6c7361926ac9240c5bc8d70a55
Fariya0909 <tislam171029@bscse.uiu.ac.bd>
2022-01-06 22:30:41 +0600

    first commit Tasnim

---
+++ src/FancyJava.java
@@ -0,0 +1,13 @@
+public class FancyJava {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+
+        int add = a + b;
+        int sub= a-b;
+
+        System.out.println("Addition: "+add);
+        System.out.println("Subtraction: "+sub);
+    }
+}
