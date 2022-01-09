77778ac9fd076ccecc997f85904a220412431fc4
Fariya0909 <tislam171029@bscse.uiu.ac.bd>
2022-01-09 23:19:58 +0600

    new changes by Tasnim

--- src/FancyJava.java
+++ src/FancyJava.java
@@ -6,8 +6,11 @@ public class FancyJava {
 
         int add = a + b;
         int sub= a-b;
+        int mul= a*b;
 
         System.out.println("Addition: "+add);
         System.out.println("Subtraction: "+sub);
+        System.out.println("Multiplication: "+mul);
+
     }
 }
